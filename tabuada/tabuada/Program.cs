9ed55f7d651bb96d3b7193dcc93e154d0572b884
Flávio Martins da Silva <85577790+flavioms473@users.noreply.github.com>
2022-01-13 17:05:34 -0300

    Add files via upload

---
+++ tabuada/tabuada/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var y = 1; y <= 10; y++)
+            {
+
+                for (var x = 1; x <= 10; x++)
+                {
+                    Console.WriteLine(y + " x " + x + " = " + (1 * x ) );
+                 }
+               
+             }
+            Console.Write("---------------------------------------");
+        }
+    }
+}
